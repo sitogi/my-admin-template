75159a49bde98e6bdd5f78191b2e9516a4975a14
sitogi <takano162@gmail.com>
2020-12-27 11:45:42 +0900

    update main page

--- src/components/organisms/CounterBoard.tsx
+++ src/components/organisms/CounterBoard.tsx
@@ -1,6 +1,8 @@
 import React, { FC } from 'react';
 import { Button, Card, Statistic } from 'semantic-ui-react';
-import './CounterBoard.css';
+import { Container } from '@chakra-ui/react';
+import 'components/organisms/CounterBoard.css';
+import EnhancedColorfulBeads from 'containers/molecules/ColorfulBeads';
 
 const BULK_UNIT = 10;
 interface Props {
@@ -16,27 +18,32 @@ const CounterBoard: FC<Props> = ({
   decrement = () => undefined,
   increment = () => undefined,
 }) => (
-  <Card>
-    <Statistic className="number-board">
-      <Statistic.Label>count</Statistic.Label>
-      <Statistic.Value>{count}</Statistic.Value>
-    </Statistic>
-    <Card.Content>
-      <div className="ui two buttons">
-        <Button color="red" onClick={decrement}>
-          -1
-        </Button>
-        <Button color="green" onClick={increment}>
-          +1
-        </Button>
-      </div>
-      <div className="fluid-button">
-        <Button fluid color="grey" onClick={() => add(BULK_UNIT)}>
-          +{BULK_UNIT}
-        </Button>
-      </div>
-    </Card.Content>
-  </Card>
+  <div>
+    <Container centerContent>
+      <Card>
+        <Statistic className="number-board">
+          <Statistic.Label>count</Statistic.Label>
+          <Statistic.Value>{count}</Statistic.Value>
+        </Statistic>
+        <Card.Content>
+          <div className="ui two buttons">
+            <Button color="red" onClick={decrement}>
+              -1
+            </Button>
+            <Button color="green" onClick={increment}>
+              +1
+            </Button>
+          </div>
+          <div className="fluid-button">
+            <Button fluid color="grey" onClick={() => add(BULK_UNIT)}>
+              +{BULK_UNIT}
+            </Button>
+          </div>
+        </Card.Content>
+      </Card>
+      <EnhancedColorfulBeads />
+    </Container>
+  </div>
 );
 
 export default CounterBoard;
--- src/components/organisms/Navigation.tsx
+++ src/components/organisms/Navigation.tsx
@@ -1,18 +1,18 @@
 /** @jsx jsx */
 import React, { FC } from 'react'; // eslint-disable-line @typescript-eslint/no-unused-vars
 import { jsx } from '@emotion/react';
-import { Box, StackDivider, VStack } from '@chakra-ui/react';
+import { Box, Divider, VStack } from '@chakra-ui/react';
 
 const Navigation: FC = () => {
   return (
-    <VStack
-      divider={<StackDivider borderColor="gray.200" />}
-      spacing={4}
-      align="stretch"
-    >
+    <VStack align="stretch">
+      <Divider />
       <Box h="40px">Users</Box>
+      <Divider />
       <Box h="40px">Orders</Box>
+      <Divider />
       <Box h="40px">Statuses</Box>
+      <Divider />
     </VStack>
   );
 };
