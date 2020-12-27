e25df54bf2ef9fd33a00ceea0a23e130137e0793
sitogi <takano162@gmail.com>
2020-12-27 11:26:31 +0900

    update Sidebar

--- src/components/organisms/Navigation.tsx
+++ src/components/organisms/Navigation.tsx
@@ -1,62 +1,19 @@
 /** @jsx jsx */
-import React, { FC, useState } from 'react'; // eslint-disable-line @typescript-eslint/no-unused-vars
+import React, { FC } from 'react'; // eslint-disable-line @typescript-eslint/no-unused-vars
 import { jsx } from '@emotion/react';
-import { useHistory } from 'react-router';
-import { Menu, MenuItemProps } from 'semantic-ui-react';
-import { PATH_ROOT } from 'containers/organisms/RouteManager';
+import { Box, StackDivider, VStack } from '@chakra-ui/react';
 
 const Navigation: FC = () => {
-  const history = useHistory();
-
-  const [activeItem, setActiveItem] = useState<string | undefined>('home');
-
   return (
-    <Menu pointing secondary vertical>
-      <Menu.Item
-        name="home"
-        active={activeItem === 'home'}
-        onClick={(
-          e: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
-          { name }: MenuItemProps,
-        ) => {
-          setActiveItem(name);
-          history.push(PATH_ROOT);
-        }}
-      />
-      <Menu.Item
-        name="settings"
-        active={activeItem === 'settings'}
-        onClick={(
-          e: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
-          { name }: MenuItemProps,
-        ) => {
-          setActiveItem(name);
-          history.push(PATH_ROOT);
-        }}
-      />
-      <Menu.Item
-        name="canvas"
-        active={activeItem === 'canvas'}
-        onClick={(
-          e: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
-          { name }: MenuItemProps,
-        ) => {
-          setActiveItem(name);
-          history.push(PATH_ROOT);
-        }}
-      />
-      <Menu.Item
-        name="logout"
-        active={activeItem === 'logout'}
-        onClick={(
-          e: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
-          { name }: MenuItemProps,
-        ) => {
-          setActiveItem(name);
-          // TODO: not implemented yet.
-        }}
-      />
-    </Menu>
+    <VStack
+      divider={<StackDivider borderColor="gray.200" />}
+      spacing={4}
+      align="stretch"
+    >
+      <Box h="40px">Users</Box>
+      <Box h="40px">Orders</Box>
+      <Box h="40px">Statuses</Box>
+    </VStack>
   );
 };
 
