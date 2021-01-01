dec0fe59f447192bbe9e5cc32b0b5f33b90d5e2e
sitogi <takano162@gmail.com>
2021-01-01 15:21:21 +0900

    Update Login page

--- src/components/pages/Login/Login.tsx
+++ src/components/pages/Login/Login.tsx
@@ -1,5 +1,7 @@
-import { Alert, AlertIcon, Button, Container, FormControl, FormLabel, Input } from '@chakra-ui/react';
-import React, { FC } from 'react';
+/** @jsx jsx */
+import React, { FC } from 'react'; // eslint-disable-line @typescript-eslint/no-unused-vars
+import { css, jsx } from '@emotion/react';
+import { Alert, AlertIcon, Button, Center, Flex, FormControl, FormLabel, Input, Spacer } from '@chakra-ui/react';
 
 interface LoginProps {
   errorMsg: string;
@@ -10,25 +12,47 @@ interface LoginProps {
 
 const Login: FC<LoginProps> = ({ errorMsg, handleOnChangeEmail, handleOnChangePassword, handleOnSubmit }) => {
   return (
-    <Container>
+    <Flex
+      css={css`
+        background-color: #f4f3ef;
+      `}
+      direction="column"
+      h="100vh"
+      w="100vw"
+      align="center"
+    >
+      <Spacer />
       {errorMsg && (
-        <Alert status="error">
-          <AlertIcon />
-          {errorMsg}
-        </Alert>
+        <Center w="800px">
+          <Alert status="error">
+            {' '}
+            <AlertIcon />
+            {errorMsg}
+          </Alert>
+        </Center>
       )}
-      <FormControl id="email">
-        <FormLabel>Email address</FormLabel>
-        <Input type="email" onChange={(event) => handleOnChangeEmail(String(event.target.value))} />
-      </FormControl>
-      <FormControl id="password">
-        <FormLabel>Password</FormLabel>
-        <Input type="password" onChange={(event) => handleOnChangePassword(String(event.target.value))} />
-      </FormControl>
-      <Button colorScheme="blue" onClick={() => handleOnSubmit()}>
-        Button
-      </Button>
-    </Container>
+      <Center w="400px" mt="10px">
+        <FormControl id="email">
+          <FormLabel>Email address</FormLabel>
+          <Input type="email" onChange={(event) => handleOnChangeEmail(String(event.target.value))} />
+        </FormControl>
+      </Center>
+      <Center w="400px" mt="10px">
+        <FormControl id="password">
+          <FormLabel>Password</FormLabel>
+          <Input type="password" onChange={(event) => handleOnChangePassword(String(event.target.value))} />
+        </FormControl>
+      </Center>
+      <Flex w="400px" mt="10px">
+        <Spacer />
+        <Center>
+          <Button colorScheme="blue" onClick={() => handleOnSubmit()}>
+            Sign In
+          </Button>
+        </Center>
+      </Flex>
+      <Spacer />
+    </Flex>
   );
 };
 
