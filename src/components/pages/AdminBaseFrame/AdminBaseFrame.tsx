0f129e336ffccde97d82217c1be814bfacaa4f30
sitogi <takano162@gmail.com>
2021-01-02 22:59:33 +0900

    Add table example

--- src/components/pages/AdminBaseFrame/AdminBaseFrame.tsx
+++ src/components/pages/AdminBaseFrame/AdminBaseFrame.tsx
@@ -5,6 +5,7 @@ import { Route, Switch } from 'react-router';
 import { PATH_ROOT } from 'components/organisms/RouteManager/RouteManager';
 import SidebarContainer from 'components/templates/Sidebar/SidebarContainer';
 import PageHeader from 'components/organisms/PageHeader/PageHeader';
+import { Box, Grid, Table, TableCaption, Tbody, Td, Text, Th, Thead, Tr } from '@chakra-ui/react';
 
 const containerCss = css`
   display: flex;
@@ -29,9 +30,72 @@ const contentsCss = css`
 `;
 
 const DummyContent = () => (
-  <div>
-    <p>あいうえお</p>
-  </div>
+  <Grid templateColumns="repeat(2, 1fr)" gap={6}>
+    <Box bg="white" pt="10px" rounded="xl" boxShadow="xl">
+      <Text ml={5} mb={5} mt={3} fontSize="20px" color="gray.400">
+        User Ranking
+      </Text>
+      <Table variant="simple" overflow="scroll">
+        <TableCaption>Imperial to metric conversion factors</TableCaption>
+        <Thead>
+          <Tr>
+            <Th>To convert</Th>
+            <Th>into</Th>
+            <Th isNumeric>multiply by</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          <Tr _hover={{ bg: 'teal.50' }}>
+            <Td>inches</Td>
+            <Td>millimetres (mm)</Td>
+            <Td isNumeric>25.4</Td>
+          </Tr>
+          <Tr _hover={{ bg: 'teal.50' }}>
+            <Td>feet</Td>
+            <Td>centimetres (cm)</Td>
+            <Td isNumeric>30.48</Td>
+          </Tr>
+          <Tr _hover={{ bg: 'teal.50' }}>
+            <Td>yards</Td>
+            <Td>metres (m)</Td>
+            <Td isNumeric>0.91444</Td>
+          </Tr>
+        </Tbody>
+      </Table>
+    </Box>
+    <Box bg="white" pt="10px" rounded="xl" boxShadow="xl">
+      <Text ml={5} mb={5} mt={3} fontSize="20px" color="gray.400">
+        User Ranking
+      </Text>
+      <Table variant="simple">
+        <TableCaption>Imperial to metric conversion factors</TableCaption>
+        <Thead>
+          <Tr>
+            <Th>To convert</Th>
+            <Th>into</Th>
+            <Th isNumeric>multiply by</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          <Tr _hover={{ bg: 'teal.50' }}>
+            <Td>inches</Td>
+            <Td>millimetres (mm)</Td>
+            <Td isNumeric>25.4</Td>
+          </Tr>
+          <Tr _hover={{ bg: 'teal.50' }}>
+            <Td>feet</Td>
+            <Td>centimetres (cm)</Td>
+            <Td isNumeric>30.48</Td>
+          </Tr>
+          <Tr _hover={{ bg: 'teal.50' }}>
+            <Td>yards</Td>
+            <Td>metres (m)</Td>
+            <Td isNumeric>0.91444</Td>
+          </Tr>
+        </Tbody>
+      </Table>
+    </Box>
+  </Grid>
 );
 
 const AdminBaseFrame: FC = () => {
