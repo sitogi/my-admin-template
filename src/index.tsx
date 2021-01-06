31e68e5e05b0fdb49502fe1bfc7ab5f09a942f58
sitogi <takano162@gmail.com>
2021-01-06 21:36:23 +0900

    Remove unused files

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-import { counterSlice } from 'features/counter';
 import { ChakraProvider } from '@chakra-ui/react';
 import * as serviceWorker from 'serviceWorker';
 import App from 'App';
@@ -11,7 +10,6 @@ import { sidebarSlice } from 'features/sidebar';
 
 const store = configureStore({
   reducer: combineReducers({
-    counter: counterSlice.reducer,
     sidebar: sidebarSlice.reducer,
   }),
 });
--- src/store.ts
+++ src/store.ts
@@ -1,9 +1,7 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
-import { counterSlice } from 'features/counter';
 import { sidebarSlice } from 'features/sidebar';
 
 const rootReducer = combineReducers({
-  counter: counterSlice.reducer,
   sidebar: sidebarSlice.reducer,
 });
 
