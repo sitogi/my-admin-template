312b68b866aeda9cde0769686a9b9da4e89652df
sitogi <takano162@gmail.com>
2021-01-20 23:12:33 +0900

    Remove react-dnd from dependencies

--- src/components/pages/Todo/Board.tsx
+++ src/components/pages/Todo/Board.tsx
@@ -1,14 +1,10 @@
 import React, { FC } from 'react';
 import { Box } from '@chakra-ui/react';
-import { DndProvider } from 'react-dnd';
-import { HTML5Backend } from 'react-dnd-html5-backend';
 import ListContainer from './ListContainer';
 
 const Board: FC = () => (
   <Box overflowY="auto" h="100%">
-    <DndProvider backend={HTML5Backend}>
-      <ListContainer />
-    </DndProvider>
+    <ListContainer />
   </Box>
 );
 
